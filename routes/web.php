dc6339205a32c82f14389caae92f859c7af83b45
Isaac Nogueira <isaacnog@Matrix.localdomain>
2022-10-10 16:56:55 -0400

    configurações iniciais

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function() {
+    return 'Hello world!';
+});
+
+Route::resource('/users', UserController::class);
